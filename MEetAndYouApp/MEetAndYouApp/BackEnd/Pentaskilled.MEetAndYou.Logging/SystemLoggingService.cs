80fd22f217bd72d5991b8172615f7fc8c2b88f6e
Brent Nishioka <brentnishioka@gmail.com>
2021-12-11 15:38:46 -0800

    Implementation of System & User logging service init

---
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.Logging/SystemLoggingService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pentaskilled.MEetAndYou.DataAccess;
+using Pentaskilled.MEetAndYou.Entities;
+
+namespace Pentaskilled.MEetAndYou.Logging
+{
+    public class SystemLoggingService : ISystemLoggingService
+    {
+        private ILogDAO _logDataAccess;
+        private SystemLog _sysLog;
+
+        public SystemLoggingService()
+        { 
+            _logDataAccess = new LogDAO();
+            _sysLog = new SystemLog();
+        }
+
+        public bool CreateNewLog(DateTime dateTime, string category, LogLevel logLevel, string message)
+        {
+            try
+            {
+                MakeLog(dateTime, category, logLevel, message);
+                PushLogToDB(_sysLog);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public SystemLog MakeLog(DateTime dateTime, string category, LogLevel logLevel, string message)
+        {
+            _sysLog.dateTime = dateTime;
+            _sysLog.category = category;
+            _sysLog.logLevel = logLevel;
+            _sysLog.message = message;
+            
+            return _sysLog;
+        }
+
+        public bool PushLogToDB(SystemLog sysLog)
+        {
+            try
+            {
+                _logDataAccess.PushLogToDB(sysLog);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.Logging/UserLoggingService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pentaskilled.MEetAndYou.DataAccess;
+using Pentaskilled.MEetAndYou.Entities;
+
+namespace Pentaskilled.MEetAndYou.Logging
+{
+    public class UserLoggingService : IUserLoggingService
+    {
+        private ILogDAO _logDataAccess;
+        private UserLog _userLog;
+
+        public UserLoggingService()
+        {
+            _logDataAccess = new LogDAO();
+            _userLog = new UserLog();
+        }
+
+        public bool CreateNewLog(DateTime dateTime, string category, LogLevel logLevel, int userId, string message)
+        {
+            try
+            {
+                MakeLog(dateTime, category, logLevel, userId, message);
+                PushLogToDB(_userLog);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public UserLog MakeLog(DateTime dateTime, string category, LogLevel logLevel, int userId, string message)
+        {
+            _userLog.dateTime = dateTime;
+            _userLog.category = category;
+            _userLog.logLevel = logLevel;
+            _userLog.message = message;
+
+            return _userLog;
+        }
+
+        public bool PushLogToDB(UserLog sysLog)
+        {
+            try
+            {
+                _logDataAccess.PushLogToDB(sysLog);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
