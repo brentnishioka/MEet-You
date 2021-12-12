4a919f8e3a704b5bfae2d9fed8981339a5fe4a8b
Gideon <gowusumsse@gmail.com>
2021-12-12 12:40:57 -0800

    Moved files to proper location

---
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.Services/Implementations/ArchiverService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pentaskilled.MEetAndYou.DataAccess
+{
+    public class ArchiverService : IArchiverService
+    {
+
+        public bool GetOldLogs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsLogOlderThan30Days(string FileName)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public bool ConsolidateOldLogs(List<Object> oldLogs)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public bool CompressOldLogs()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public bool RelocateOldLogs()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public int GetArchiveCount()
+        {
+            throw new NotImplementedException();
+
+        }
+
+    }
+}
