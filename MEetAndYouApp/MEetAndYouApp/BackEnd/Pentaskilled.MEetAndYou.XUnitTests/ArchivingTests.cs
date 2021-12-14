732327ee8935d4a52f221efe6baa54e4f5eff911
Brent Nishioka <brentnishioka@gmail.com>
2021-12-13 18:52:27 -0800

    Update ArchivingTests.cs

--- MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.XUnitTests/ArchivingTests.cs
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.XUnitTests/ArchivingTests.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Pentaskilled.MEetAndYou.DataAccess;
 using Pentaskilled.MEetAndYou.Entities;
 using Pentaskilled.MEetAndYou.Logging;
+using Pentaskilled.MEetAndYou.Managers;
 using Pentaskilled.MEetAndYou.Services;
 using Pentaskilled.MEetAndYou.Services.Contracts;
 using Pentaskilled.MEetAndYou.Services.Implementations;
@@ -114,6 +115,13 @@ namespace Pentaskilled.MEetAndYou.XUnitTests
             Assert.False(log1.ToString().Equals(log2.ToString()));
         }
 
+        [Fact]
+        public void ArchiveOldLogsTest()
+        {
+            ArchiveManager archMan = new ArchiveManager();
+            bool archOldLogsExpected = true;
 
+            Assert.Equal(archOldLogsExpected, archMan.ArchiveOldLogs());
+        }
     }
 }
