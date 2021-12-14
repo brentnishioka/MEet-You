67e1f22a6bde13f185329ba077f6a4238a25a5fe
Gideon <gowusumsse@gmail.com>
2021-12-13 19:03:43 -0800

    Amended Archive.config

--- MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.DomainModels/ArchiveConfig.cs
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.DomainModels/ArchiveConfig.cs
@@ -4,11 +4,26 @@ namespace Pentaskilled.MEetAndYou.DomainModels
 {
     public class ArchiveConfig
     {
+
+        static string strDirectory = Environment.CurrentDirectory.ToString();
+        static string firstDir = System.IO.Directory.GetParent(strDirectory).ToString();
+
+        static string secondDir = System.IO.Directory.GetParent(firstDir).ToString();
+
+        static string thirdDir = System.IO.Directory.GetParent(secondDir).ToString();
+        static string l = System.IO.Directory.GetParent(thirdDir).ToString();
+
+
+        static string buffLocation = System.IO.Path.Combine(l, "senior-project-buffer");
+        static string archiveLocation = System.IO.Path.Combine(l, "senior-project-archives");
+
+    
+
         private const string DATE_TIME_FORMAT = "yyyy_MM_dd";
         private const string BUFFER_EXTENSION = "_MEetAndYouarchive.csv";
         private const string ARCHIVE_EXTENSION = "_MEetAndYouarchive.zip";
-        private const string BUFFER_LOCATION = @"C:\Users\Raymo\Music\senior-project-buffer";
-        private const string ARCHIVE_LOCATION = @"C:\Users\Raymo\Music\senior-project-archives";
+        private string BUFFER_LOCATION = buffLocation;
+        private string ARCHIVE_LOCATION = archiveLocation;
 
         public string GetDateTimeFormat()
         {
--- MEetAndYouApp/MEetAndYouApp/BackEnd/TestClass.cs
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/TestClass.cs
@@ -4,7 +4,36 @@ class TestClass
 {
     static void Main(string[] args)
     {
-        // Display the number of command line arguments.
-        Console.WriteLine("Hello world");
+        string str_directory = Environment.CurrentDirectory.ToString();
+        Console.WriteLine(str_directory);
+
+        Console.WriteLine(System.IO.Directory.GetParent(str_directory).ToString());
+        string t = System.IO.Directory.GetParent(str_directory).ToString();
+
+
+        Console.WriteLine(System.IO.Directory.GetParent(t).ToString());
+
+        string r = System.IO.Directory.GetParent(t).ToString();
+
+        Console.WriteLine(System.IO.Directory.GetParent(r).ToString());
+
+
+
+        string m = System.IO.Directory.GetParent(r).ToString();
+
+        Console.WriteLine(System.IO.Directory.GetParent(m).ToString());
+
+
+        string l = System.IO.Directory.GetParent(m).ToString();
+
+        Console.WriteLine(System.IO.Directory.GetParent(m).ToString());
+
+        string g = System.IO.Path.Combine(l, "senior-project-archives");
+
+        if (!Directory.Exists(g))
+        {
+            Directory.CreateDirectory(g);
+            Console.WriteLine(g);
+        }
     }
 }
