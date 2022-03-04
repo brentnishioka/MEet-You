bd9fc67d96af4d81b850a78b37455c6cf6872738
rayRay <raymond13gggg@gmail.com>
2022-03-04 11:07:57 -0800

    AuthnService implments interface

--- MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.Services/Implementations/AuthnService.cs
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.Services/Implementations/AuthnService.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Pentaskilled.MEetAndYou.Services.Contracts;
 
 namespace Pentaskilled.MEetAndYou.Services.Implementations
 {
-    public class AuthnService
+    public class AuthnService : IAuthnService
     {
-        
+        public string generateOTP()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string generateToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool validateOTP()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool validateUserInput(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
