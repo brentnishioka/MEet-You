2e40858a1f6ca7935ada270641d4368691b03425
Lunastra <ln24630@gmail.com>
2022-03-04 21:09:42 -0800

    Bring implemetation folder back

---
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/PentaSkilled.MEetAndYou.Authorization/Implementation/AuthorizationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PentaSkilled.MEetAndYou.Authorization.Implmentation
+{
+    public class AuthorizationService
+    {
+        bool IsAuthenticated(string randomToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
