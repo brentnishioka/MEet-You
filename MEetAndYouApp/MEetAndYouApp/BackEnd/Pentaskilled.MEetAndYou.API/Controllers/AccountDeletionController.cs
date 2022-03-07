88866e9e9e069628018ae59952c5ced36ebb753e
Brent Nishioka <brentnishioka@gmail.com>
2022-03-06 20:22:22 -0800

    Web api init

---
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.API/Controllers/AccountDeletionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pentaskilled.MEetAndYou.Managers;
+
+namespace Pentaskilled.MEetAndYou.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountDeletionController : ControllerBase
+    {
+        private AccountDeletionManager _accDelManager;
+
+        public AccountDeletionController()
+        {
+            _accDelManager = new AccountDeletionManager();
+        }
+
+        [HttpDelete("self")]
+        public IActionResult DeleteUserAccount(IFormCollection formCollection)
+        {
+            // calls DeleteUser from UMmanager
+            return Ok(_accDelManager.SelfDeleteUser());
+        }
+    }
+}
