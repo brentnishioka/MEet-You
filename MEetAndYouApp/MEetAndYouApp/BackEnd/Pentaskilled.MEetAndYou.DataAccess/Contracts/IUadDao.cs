46f2437c59bca3861c7fb89a2bf8a8f23f6c4a38
rayRay <raymond13gggg@gmail.com>
2022-03-06 22:36:58 -0800

    uad files

---
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.DataAccess/Contracts/IUadDao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pentaskilled.MEetAndYou.Entities;
+
+namespace Pentaskilled.MEetAndYou.DataAccess.Contracts
+{
+    public interface IUadDao
+    {
+        List<SessionViews> getMostVisitedView();
+        List<SessionViews> getAvgViewTime();
+        Dictionary<DateTime, int> getDailyLogin();
+
+        Dictionary<DateTime, int> getDailyRegistration();
+
+        
+
+        
+    }
+}
