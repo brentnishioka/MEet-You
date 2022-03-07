f9c26e93be03322dc189f167f58a33d6602c045a
Joshua Ramos <jdcramos8@gmail.com>
2022-03-06 23:04:09 -0800

    Added parsing unit test for WeatherService

--- MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.XUnitTests/WeatherTests.cs
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.XUnitTests/WeatherTests.cs
@@ -1,6 +1,7 @@
 ﻿using Xunit;
 using System;
 using Pentaskilled.MEetAndYou.DataAccess.Implementation;
+using Pentaskilled.MEetAndYou.Services.Implementation;
 
 namespace Pentaskilled.MEetAndYou.XUnitTests
 {
@@ -17,6 +18,23 @@ namespace Pentaskilled.MEetAndYou.XUnitTests
             geoJsonString = weatherDAO.GeoCoderAPI(cityName, countryName);
 
             Assert.Equal("{\"name\":\"London\",\"lat\":37.1289771,\"lon\":-84.0832646,\"country\":\"US\",\"state\":\"Kentucky\"}", geoJsonString);
+        }
+
+        [Fact]
+        public void ServiceLatLonTest()
+        {
+            WeatherDAO weatherDAO = new WeatherDAO();
+            WeatherService weatherService = new WeatherService();
+            string cityName = "London";
+            string countryName = "KY";
+            string geoJsonString;
+            string[] coordinates;
+
+            geoJsonString = weatherDAO.GeoCoderAPI(cityName, countryName);
+            coordinates = weatherService.ParseLatLong(geoJsonString);
+
+            Assert.Equal("37.12898", coordinates[0]);
+            Assert.Equal("-84.08327", coordinates[1]);
         }
     }
 }
