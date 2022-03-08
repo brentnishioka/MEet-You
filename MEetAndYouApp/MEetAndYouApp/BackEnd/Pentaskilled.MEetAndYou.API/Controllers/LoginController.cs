59123322c90dce858f4bf6a1785c9bce17ce343c
Viviand17 <vivian.dinh03@student.csulb.edu>
2022-03-07 18:33:56 -0800

    Tried to make a login controller lol

---
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.API/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pentaskilled.MEetAndYou.Managers;
+using Pentaskilled.MEetAndYou.Entities;
+using Newtonsoft.Json.Linq;
+
+namespace Pentaskilled.MEetAndYou.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private AuthnManager _authnManager;
+
+        public LoginController()
+        {
+            _authnManager = new AuthnManager();
+        }
+
+        [HttpPost("/login")]
+        public string Login([FromBody] JObject userInfo)
+        {
+            var email = userInfo["email"].ToString();
+            var password = userInfo["password"].ToString();
+            return _authnManager.AuthenticateUser(email, password);
+        }
+    }
+}
