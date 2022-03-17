1d2be75a29d5738d31d409fa07399945059751e4
Joshua Ramos <jdcramos8@gmail.com>
2022-03-17 13:03:45 -0700

    initialize demo API

---
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/WeatherDemo/Controllers/TestController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherDemo.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TestController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<TestController> _logger;
+
+    public TestController(ILogger<TestController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    [Route("Vehicle")]
+    public Object Get1()
+    {
+        var car = new { Model = "Honda", Year = 1999 };
+        return car;
+    }
+
+    //[HttpGet(Name = "CalculateFibonachi")]
+    //public Object GetStudent()
+    //{
+    //    var student = new { Name = "Joshua", Age = 42};
+    //    return student;
+    //}
+
+    [HttpPost]
+    [Route("Hello")]
+    public void Hello()
+    {
+
+    }
+}
+
