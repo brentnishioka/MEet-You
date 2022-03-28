5061bfe99b697ff3b62fe194b2d731832b930be0
Teostra <notice_me_senpai22@protonmail.com>
2022-03-28 13:36:23 -0700

    Remove DB connection String from Context
    Move DB connection string to appsettings.json for dependency injection.

--- MEetAndYouApp/MEetAndYouApp/BackEnd/WeatherDemo/Program.cs
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/WeatherDemo/Program.cs
@@ -1,6 +1,14 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Pentaskilled.MEetAndYou.Entities.DBModels;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Add ASPNETCoreDemoDBContext services. (Dependency Injection for database)
+builder.Services.AddDbContext<MEetAndYouDBContext>(
+    options => options.UseSqlServer(System.Configuration.ConfigurationManager.ConnectionStrings["MEetAndYouDatabase"].ConnectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
