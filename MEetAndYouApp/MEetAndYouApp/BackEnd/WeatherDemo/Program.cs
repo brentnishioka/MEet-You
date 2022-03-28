04e56b1736c2c99d2927ec8a90a2dc2563851fa2
Teostra <notice_me_senpai22@protonmail.com>
2022-03-28 16:40:41 -0700

    Add app.config for DepenInjection
    Add an app.config file for dependency injection.
    Moved the connection string app.config file and removed OnConfiguration from DBContext

--- MEetAndYouApp/MEetAndYouApp/BackEnd/WeatherDemo/Program.cs
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/WeatherDemo/Program.cs
@@ -1,19 +1,20 @@
 ﻿using System.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Pentaskilled.MEetAndYou.Entities.DBModels;
+using Microsoft.Extensions.Configuration;                   //Ask to see if it is approved
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 // Add ASPNETCoreDemoDBContext services. (Dependency Injection for database)
-//builder.Services.AddDbContext<MEetAndYouDBContext>(
-//    options => options.UseSqlServer(System.Configuration.ConfigurationManager.ConnectionStrings["MEetAndYouDatabase"].ConnectionString));
-//builder.Services.AddDbContext<MEetAndYouDBContext>(options =>
-//     options.UseSqlServer(
-//        Configuration.GetConnectionString("MEetAndYouDatabase")));
+var connection =
+    System.Configuration.ConfigurationManager.
+    ConnectionStrings["MEetAndYouDatabase"].ConnectionString;
 
-builder.Services.AddDbContext<MEetAndYouDBContext>();
+builder.Services.AddDbContext<MEetAndYouDBContext>(options =>
+     options.UseSqlServer(connection));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
