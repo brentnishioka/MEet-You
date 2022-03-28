ac87b775b438b95dcc7a2522144c21c65a0e1407
Teostra <notice_me_senpai22@protonmail.com>
2022-03-28 15:54:58 -0700

    Add EventController and DB context
    Event controller before dependency injection.

--- MEetAndYouApp/MEetAndYouApp/BackEnd/WeatherDemo/Program.cs
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/WeatherDemo/Program.cs
@@ -7,8 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 // Add ASPNETCoreDemoDBContext services. (Dependency Injection for database)
-builder.Services.AddDbContext<MEetAndYouDBContext>(
-    options => options.UseSqlServer(System.Configuration.ConfigurationManager.ConnectionStrings["MEetAndYouDatabase"].ConnectionString));
+//builder.Services.AddDbContext<MEetAndYouDBContext>(
+//    options => options.UseSqlServer(System.Configuration.ConfigurationManager.ConnectionStrings["MEetAndYouDatabase"].ConnectionString));
+//builder.Services.AddDbContext<MEetAndYouDBContext>(options =>
+//     options.UseSqlServer(
+//        Configuration.GetConnectionString("MEetAndYouDatabase")));
+
+builder.Services.AddDbContext<MEetAndYouDBContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
