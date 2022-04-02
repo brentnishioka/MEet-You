8047c0b37f9229c54eb47cb0028ea1dacdec4b3b
Teostra <notice_me_senpai22@protonmail.com>
2022-04-01 23:56:14 -0700

    Add a controller for Login

---
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/WeatherDemo/Controllers/LoginController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+    }
+}
