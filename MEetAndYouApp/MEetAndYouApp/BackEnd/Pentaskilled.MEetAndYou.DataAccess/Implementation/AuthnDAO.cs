be338e9d3f757c45377e8f036852a89d419ba8a8
Teostra <notice_me_senpai22@protonmail.com>
2022-04-02 15:41:13 -0700

    Add Login controller
    Fixed AuthNManagers
    Add controller for logging in.

--- MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.DataAccess/Implementation/AuthnDAO.cs
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.DataAccess/Implementation/AuthnDAO.cs
@@ -27,7 +27,7 @@ namespace Pentaskilled.MEetAndYou.DataAccess
             try
             {
                 using (SqlConnection connection = new SqlConnection(_connectionString))
-                using (SqlCommand command = new SqlCommand("[MEetAndYou].[ValidateCredentialsInDB](@email, @password)", connection))
+                using (SqlCommand command = new SqlCommand("SELECT [MEetAndYou].[ValidateCredentialsInDB](@email, @password)", connection))
                 {
                     command.CommandType = CommandType.Text;
                     command.Parameters.Add("@email", SqlDbType.VarChar).Value = email;
@@ -47,6 +47,7 @@ namespace Pentaskilled.MEetAndYou.DataAccess
             return Task.FromResult(Convert.ToBoolean(rowsReturned)); 
         }
 
+        // Todo: GetPhoneNumber function or store procedure does not exist in the database
         public Task<string> GetPhoneNum(string email, string password)
         {
             _connectionString = GetConnectionString();
--- MEetAndYouApp/MEetAndYouApp/BackEnd/WeatherDemo/Controllers/LoginController.cs
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/WeatherDemo/Controllers/LoginController.cs
@@ -1,11 +1,24 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Pentaskilled.MEetAndYou.Managers;
 
 namespace WeatherDemo.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class LoginController : ControllerBase
     {
+
+        //Method to login
+        [HttpPost]
+        [Route("Signin")]
+        public Object SignIn(string userEmail, string userPassword)
+        {
+            AuthnManager authnManager = new AuthnManager();
+            string token = authnManager.AuthenticateUser(userEmail, userPassword);
+
+            return Ok(token);
+            //return new ObjectResult(new { Value = token });
+        }
     }
 }
