6017fd4468fa84a3689480d6b787ac896dc5a933
Teostra <notice_me_senpai22@protonmail.com>
2022-04-20 20:21:24 -0700

    Add code for testing EventAPI

---
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/WeatherDemo/Controllers/SuggestionController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Pentaskilled.MEetAndYou.Managers;
+
+namespace Pentaskiled.MEetAndYou.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuggestionController : ControllerBase
+    {
+        private readonly SuggestionManager _suggestionManager;
+
+        public SuggestionController(SuggestionManager suggestionController)
+        {
+            _suggestionManager = suggestionController;
+        }
+
+        [HttpGet]
+        [Route("Suggestion/GetSuggestion")]
+        public ActionResult<JObject> GetEventCat()
+        {
+            JObject result = _suggestionManager.GetEventByCategory();
+            return result;
+        }
+    }
+}
