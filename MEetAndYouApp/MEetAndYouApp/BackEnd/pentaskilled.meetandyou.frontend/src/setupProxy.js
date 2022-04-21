727d2def7de8123daacdf032aae108bf41b6ab4c
Teostra <notice_me_senpai22@protonmail.com>
2022-04-21 13:58:57 -0700

    Rename WeatherDemo

--- MEetAndYouApp/MEetAndYouApp/BackEnd/pentaskilled.meetandyou.frontend/src/setupProxy.js
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/pentaskilled.meetandyou.frontend/src/setupProxy.js
@@ -7,7 +7,7 @@ const context = [
 module.exports = function (app) {
     const appProxy = createProxyMiddleware(context, {
         target: 'https://localhost:9000',
-        secure: false
+        secure: true
     });
 
     app.use(appProxy);
