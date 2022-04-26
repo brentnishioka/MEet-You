7e2b5d95ff6965f658373ea46a25919cabd639b0
Brent Nishioka <brentnishioka@gmail.com>
2022-04-26 15:46:00 -0700

    Readded constructor + jsonignore

--- MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.Entities/DBModels/Event.cs
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.Entities/DBModels/Event.cs
@@ -11,6 +11,16 @@ namespace Pentaskilled.MEetAndYou.Entities.DBModels
             Itineraries = new HashSet<Itinerary>();
         }
 
+        public Event(int newID, string name, string description, string address, float price, DateTime dateTime)
+        {
+            EventId = newID;
+            EventName = name;
+            Description = description;
+            Address = address;
+            Price = price;
+            EventDate = dateTime;
+        }
+
         public int EventId { get; set; }
         public string EventName { get; set; }
         public string Description { get; set; }
@@ -18,6 +28,8 @@ namespace Pentaskilled.MEetAndYou.Entities.DBModels
         public double? Price { get; set; }
         public DateTime? EventDate { get; set; }
 
+        // Temporary method to solve circular reference between Itinerary and Event
+        [System.Text.Json.Serialization.JsonIgnore]
         public virtual ICollection<Category> CategoryNames { get; set; }
         public virtual ICollection<Itinerary> Itineraries { get; set; }
     }
