a8c89803b36e027858fb282763c3abbe602988ab
Brent Nishioka <brentnishioka@gmail.com>
2022-04-28 14:39:07 -0700

    Added more unit tests for RatingDAO

--- MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.XUnitTests/RatingTests.cs
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.XUnitTests/RatingTests.cs
@@ -14,12 +14,42 @@ namespace Pentaskilled.MEetAndYou.XUnitTests
     public class RatingTests
     {
         [Theory]
-        [InlineData(1, 2, 4)]
+        [InlineData(4, 7, 4)]
         public void AddRatingSuccessTest(int eventID, int itineraryID, int userRating)
         {
             IRatingDAO _ratingDAO = new RatingDAO(new MEetAndYouDBContext());
             UserEventRating rating = new UserEventRating(eventID, itineraryID, userRating);
             BaseResponse result = _ratingDAO.AddRatingInDBAsync(rating).Result;
+            Assert.True(result.IsSuccessful);
+        }
+
+        [Theory]
+        [InlineData(4, 7, 3)]
+        public void ModifyRatingSuccessTest(int eventID, int itineraryID, int userRating)
+        {
+            IRatingDAO _ratingDAO = new RatingDAO(new MEetAndYouDBContext());
+            UserEventRating rating = new UserEventRating(eventID, itineraryID, userRating);
+            BaseResponse result = _ratingDAO.ModifyRatingInDBAsync(rating).Result;
+            Assert.True(result.IsSuccessful);
+        }
+
+        [Theory]
+        [InlineData(6, "Enjoyed the first world wonder, the other six were very underwhelming.")]
+        public void AddItineraryNoteSuccessTest(int itineraryID, string noteContent)
+        {
+            IRatingDAO _ratingDAO = new RatingDAO(new MEetAndYouDBContext());
+            ItineraryNote note = new ItineraryNote(itineraryID, noteContent);
+            BaseResponse result = _ratingDAO.AddNoteInDBAsync(note).Result;
+            Assert.True(result.IsSuccessful);
+        }
+
+        [Theory]
+        [InlineData(6, "")]
+        public void ModifyItineraryNoteSuccessTest(int itineraryID, string noteContent)
+        {
+            IRatingDAO _ratingDAO = new RatingDAO(new MEetAndYouDBContext());
+            ItineraryNote note = new ItineraryNote(itineraryID, noteContent);
+            BaseResponse result = _ratingDAO.ModifyNoteInDBAsync(note).Result;
             Assert.True(result.IsSuccessful);
         }
     }
