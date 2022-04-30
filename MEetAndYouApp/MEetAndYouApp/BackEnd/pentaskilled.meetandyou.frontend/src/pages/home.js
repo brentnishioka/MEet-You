fa0449d1a0d901c5dc7b1b21b8476e44c203a839
Teostra <notice_me_senpai22@protonmail.com>
2022-04-30 01:26:46 -0700

    Change home file

--- MEetAndYouApp/MEetAndYouApp/BackEnd/pentaskilled.meetandyou.frontend/src/pages/home.js
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/pentaskilled.meetandyou.frontend/src/pages/home.js
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { useNavigate } from "react-router-dom";
 
-const Home = () => {
+export default function Home() {
+    const [city, setCity] = useState("");
+    const [date, setDate] = useState("");
+    const [state, setState] = useState("");
+    const navigate = useNavigate();
+
+    const toCategories = () => {
+        navigate("/suggestions", {
+            state: {
+                city: city,
+                date: date,
+                state: state,
+            }
+        })
+    };
+
     return (
-        <div
-            style={{
-                display: 'flex',
-                justifyContent: 'Left',
-                alignItems: 'Left',
-                height: '100vh'
-            }}
-        >
-            <h1>home page</h1>
-        </div>
-    );
-};
+        <div className="homepage-wrapper">
+            <h1>Enter to start planning</h1>
+            <form>
+                <label>
+                    <p>Enter City</p>
+                    <input type="text" placeholder="City" onChange={e => setCity(e.target.value)} />
+                </label>
 
-export default Home;
+                <label>
+                    <p>Enter State</p>
+                    <input type="text" placeholder="State" onChange={e => setState(e.target.value)} />
+                </label>
+
+                <label>
+                    <p>Enter Date</p>
+                    <input type="date" onChange={e => setDate(e.target.value)} />
+                </label>
+
+                <div>
+                    <button type="button" onClick={() => { toCategories() }}>Next</button>
+                </div>
+            </form>
+        </div>
+    )
+}
