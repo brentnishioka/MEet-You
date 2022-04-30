57a612c6318324c4594578cae960853f53dba224
Vivian Dinh <vivian.dinh03@student.csulb.edu>
2022-04-30 02:13:33 -0700

    modified events

--- MEetAndYouApp/MEetAndYouApp/BackEnd/pentaskilled.meetandyou.frontend/src/pages/events.js
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/pentaskilled.meetandyou.frontend/src/pages/events.js
@@ -12,24 +12,36 @@ export default function Events() {
     const category = location.state.categories[0].label;
 
     
-    const [eventSuggestions, getSuggestions] = useState([]);
+    const [eventSuggestions, setSuggestions] = useState([]);
 
     console.log(location.state.categories[0].label)
 
 
     const fetchSuggestions = async (category) => {
         await fetch("https://localhost:9000/GetEvent?category=" + location.state.categories[0].label + "&location=" + city + " " + state + "&date= " + date)
-            .then(response => response.text())
+            .then(response => setSuggestions(response.json()))
             .then(body => console.log(body))
     }
+    
 
     useEffect(() => {
         fetchSuggestions();
     }, [])
 
+    console.log("TEST LONG: ", eventSuggestions)
+    const { data } = eventSuggestions;
     return (
-        < div >
-            LONGLONGLONG DISPLAY YOUR SUGGESTIONS HERE
+        <div>
+            {/*{data.map((item) => (
+                <div>
+                    <h1>{item.eventName}</h1>
+                    <p>{item.description}</p>
+                    <p>{item.address}</p>
+                    <p>{item.eventDate}</p>
+                </div>
+            ))}*/}
+            {eventSuggestions.data}
+            
         </div>
-    )
+    );
 }
