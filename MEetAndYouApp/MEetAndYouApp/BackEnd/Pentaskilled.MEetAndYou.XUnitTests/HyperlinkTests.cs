89e69aa802fa2ede439be3249503b358750a17f8
Joshua Ramos <jdcramos8@gmail.com>
2022-04-30 00:53:43 -0700

    Added unit tests for HyperlinkManager add/remove

--- MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.XUnitTests/HyperlinkTests.cs
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.XUnitTests/HyperlinkTests.cs
@@ -10,6 +10,7 @@ using Newtonsoft.Json.Linq;
 using Pentaskilled.MEetAndYou.DataAccess.Implementation;
 using Pentaskilled.MEetAndYou.Entities.DBModels;
 using Pentaskilled.MEetAndYou.Entities.Models;
+using Pentaskilled.MEetAndYou.Managers.Implementation;
 using Pentaskilled.MEetAndYou.Services.Implementation;
 using Xunit;
 using Xunit.Abstractions;
@@ -41,19 +42,19 @@ namespace Pentaskilled.MEetAndYou.XUnitTests
         [InlineData("jdcramos@gmail.com")]      // Email exists in DB
         public async void AddExistingUserTest(string email)
         {
-            //Arrange
+            // Arrange
             HyperlinkDAO hyperlinkDAO = new HyperlinkDAO(_dbContext);
             UserAccountRecordResponse user;
             HyperlinkResponse hyperResponse;
             Random rand = new Random();
 
-            //Act
+            // Act
             _output.WriteLine("Adding user to itinerary....");
             user = await hyperlinkDAO.GetUserAccountRecordAsync(email);
             hyperResponse = await hyperlinkDAO.AddUserToItineraryAsync(user.Data, 9, "View");
             _output.WriteLine(hyperResponse.Message);
 
-            //Assert 
+            // Assert 
             Assert.True(hyperResponse.IsSuccessful);
         }
 
@@ -62,19 +63,19 @@ namespace Pentaskilled.MEetAndYou.XUnitTests
         [InlineData("DoesNotExist@email.com")]  // Email does NOT exist in DB
         public async void AddNonExistingUserTest(string email)
         {
-            //Arrange
+            // Arrange
             HyperlinkDAO hyperlinkDAO = new HyperlinkDAO(_dbContext);
             UserAccountRecordResponse user;
             HyperlinkResponse hyperResponse;
             Random rand = new Random();
 
-            //Act
+            // Act
             _output.WriteLine("Adding user to itinerary....");
             user = await hyperlinkDAO.GetUserAccountRecordAsync(email);
             hyperResponse = await hyperlinkDAO.AddUserToItineraryAsync(user.Data, 9, "View");
             _output.WriteLine(hyperResponse.Message);
 
-            //Assert 
+            // Assert 
             Assert.False(hyperResponse.IsSuccessful);
         }
 
@@ -83,11 +84,11 @@ namespace Pentaskilled.MEetAndYou.XUnitTests
         [InlineData("jdcramos@gmail.com")]      // Email exists in DB
         public async void GetExistingUserTest(string email)
         {
-            //Arrange
+            // Arrange
             HyperlinkDAO hyperlinkDAO = new HyperlinkDAO(_dbContext);
             UserAccountRecord userAccountRecord = new UserAccountRecord();
 
-            //Act
+            // Act
             _output.WriteLine("Pulling User Account Record....");
             UserAccountRecordResponse response = await hyperlinkDAO.GetUserAccountRecordAsync(email);
             _output.WriteLine(response.Message);
@@ -101,11 +102,11 @@ namespace Pentaskilled.MEetAndYou.XUnitTests
         [InlineData("DoesNotExist@email.com")]  // Email does NOT exist in DB
         public async void GetNonExistingUserTest(string email)
         {
-            //Arrange
+            // Arrange
             HyperlinkDAO hyperlinkDAO = new HyperlinkDAO(_dbContext);
             UserAccountRecord userAccountRecord = new UserAccountRecord();
 
-            //Act
+            // Act
             _output.WriteLine("Pulling User Account Record....");
             UserAccountRecordResponse response = await hyperlinkDAO.GetUserAccountRecordAsync(email);
             _output.WriteLine(response.Message);
@@ -119,15 +120,15 @@ namespace Pentaskilled.MEetAndYou.XUnitTests
         [InlineData(3, 5)]  // User owns the itinerary
         public async void UserDoesOwnTest(int userID, int itineraryID)
         {
-            //Arrange
+            // Arrange
             HyperlinkDAO HyperlinkDAO = new HyperlinkDAO(_dbContext);
 
-            //Act
+            // Act
             _output.WriteLine("Checking owner....");
             HyperlinkResponse response = await HyperlinkDAO.isUserOwnerAsync(userID, itineraryID);
             _output.WriteLine(response.Message);
 
-            //Assert
+            // Assert
             Assert.True(response.IsSuccessful);
         }
 
@@ -136,15 +137,15 @@ namespace Pentaskilled.MEetAndYou.XUnitTests
         [InlineData(2, 5)]  // User does NOT own the itinerary
         public async void UserDoesNotOwnTest(int userID, int itineraryID)
         {
-            //Arrange
+            // Arrange
             HyperlinkDAO HyperlinkDAO = new HyperlinkDAO(_dbContext);
 
-            //Act
+            // Act
             _output.WriteLine("Checking owner....");
             HyperlinkResponse response = await HyperlinkDAO.isUserOwnerAsync(userID, itineraryID);
             _output.WriteLine(response.Message);
 
-            //Assert
+            // Assert
             Assert.False(response.IsSuccessful);
         }
 
@@ -153,19 +154,19 @@ namespace Pentaskilled.MEetAndYou.XUnitTests
         [InlineData("jdcramos@gmail.com")]      // Email exists in DB
         public async void RemoveExistingUserTest(string email)
         {
-            //Arrange
+            // Arrange
             HyperlinkDAO hyperlinkDAO = new HyperlinkDAO(_dbContext);
             UserAccountRecordResponse user;
             HyperlinkResponse hyperResponse;
             Random rand = new Random();
 
-            //Act
+            // Act
             _output.WriteLine("Removing user from itinerary....");
             user = await hyperlinkDAO.GetUserAccountRecordAsync(email);
             hyperResponse = await hyperlinkDAO.RemoveUserFromItineraryAsync(user.Data, 9, "View");
             _output.WriteLine(hyperResponse.Message);
 
-            //Assert 
+            // Assert 
             Assert.True(hyperResponse.IsSuccessful);
         }
 
@@ -174,20 +175,56 @@ namespace Pentaskilled.MEetAndYou.XUnitTests
         [InlineData("DoesNotExist@email.com")]  // Email does NOT exist in DB
         public async void RemoveNonExistingUserTest(string email)
         {
-            //Arrange
+            // Arrange
             HyperlinkDAO hyperlinkDAO = new HyperlinkDAO(_dbContext);
             UserAccountRecordResponse user;
             HyperlinkResponse hyperResponse;
             Random rand = new Random();
 
-            //Act
+            // Act
             _output.WriteLine("Removing user from itinerary....");
             user = await hyperlinkDAO.GetUserAccountRecordAsync(email);
             hyperResponse = await hyperlinkDAO.RemoveUserFromItineraryAsync(user.Data, 9, "View");
             _output.WriteLine(hyperResponse.Message);
 
-            //Assert 
+            // Assert 
             Assert.False(hyperResponse.IsSuccessful);
+        }
+
+        // Test to add user to itinerary in HyperlinkManager
+        [Theory]
+        [InlineData(8, 9, "viviand2465@gmail.com", "View")]
+        public async void AddUserManagerTest(int userID, int itineraryID, string email, string permission)
+        {
+            // Arrange
+            HyperlinkDAO hyperlinkDAO = new HyperlinkDAO(_dbContext);
+            HyperlinkManager hyperlinkManager = new HyperlinkManager(hyperlinkDAO, _dbContext);
+
+            // Act
+            _output.WriteLine("Adding User from HyperlinkManager....");
+            HyperlinkResponse response = await hyperlinkManager.AddUserToItineraryAsync(userID, itineraryID, email, permission);
+            _output.WriteLine(response.Message);
+
+            // Assert
+            Assert.True(response.IsSuccessful);
+        }
+
+        // Test to remove user to itinerary in HyperlinkManager
+        [Theory]
+        [InlineData(8, 9, "viviand2465@gmail.com", "View")]
+        public async void RemoveUserManagerTest(int userID, int itineraryID, string email, string permission)
+        {
+            // Arrange
+            HyperlinkDAO hyperlinkDAO = new HyperlinkDAO(_dbContext);
+            HyperlinkManager hyperlinkManager = new HyperlinkManager(hyperlinkDAO, _dbContext);
+
+            // Act
+            _output.WriteLine("Removing User from HyperlinkManager....");
+            HyperlinkResponse response = await hyperlinkManager.RemoveUserFromItineraryAsync(userID, itineraryID, email, permission);
+            _output.WriteLine(response.Message);
+
+            // Assert
+            Assert.True(response.IsSuccessful);
         }
     }
 }
