9abc14d87477dbcd26840e5656b984b9c16751e1
Vivian Dinh <vivian.dinh03@student.csulb.edu>
2022-04-30 20:54:39 -0700

    calendar changes

--- MEetAndYouApp/MEetAndYouApp/BackEnd/pentaskilled.meetandyou.frontend/src/pages/mycalendar.js
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/pentaskilled.meetandyou.frontend/src/pages/mycalendar.js
@@ -8,48 +8,63 @@ import 'react-calendar/dist/Calendar.css';
 
 
 function MyCalendar() {
-    const [value, onChange] = useState(new Date());
+    const [date, setDate] = useState(new Date());
     const [itinerary, setItinerary] = useState([]);
-    const [date, makeDate] = useState("");
 
-    function createDateString() {
-        let dateString = value.getFullYear() + "-" + value.getMonth() + "-" + value.getDate();
-        makeDate(dateString);
-        console.log("DATE STRING:", dateString);
-    }
-    console.log("value test: ", value)
+    const fetchItineraryAndDisplay = async () => { //want to call this function everytime the user clicks a new day (convert to dateString first)
+        const dateString = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
 
-    //var user = sessionStorage.getItem("userID")
-
-    const fetchItinerary = async () => {
-        makeDate(value)
-        console.log("DATE TEST", date);
-        //const formData = new FormData()
-        var id = 3
+        var id = 9
         var requestOptions = {
             method: "POST",
             headerss: {
                 'Content-type': 'application/json',
                 'Accept': 'application/json, text/plain, */*'
             },
-            //body: JSON.stringify({userID: 3}),
             mode: 'cors'
         };
 
-        //.https://localhost:9000/Calendar?userID=9&date=2022-04-28
-        await fetch(`https://localhost:9000/Calendar?userID=`+id + "&date=" + date, requestOptions).then(response => response.text())
-            .then(body => console.log(body))
-    }
+        await fetch(`https://localhost:9000/Calendar?userID=` + id + "&date=" + dateString, requestOptions)
+            //.then(response => response.text())
+            //.then(body => console.log(body))
+            .then(response => response.json())
+            .then(response => setItinerary(response.data))
+            
+        
+        console.log(itinerary);
 
-    useEffect(() => {
-        fetchItinerary();
-    }, [])
+        const rows = itinerary.map(item => (
+            <tr>
+                <td align='center'>{item.eventName}</td>
+                <td align='center'>{item.address}</td>
+                <td align='center'>{item.eventDate}</td>
+            </tr>
+        ));
+
+        console.log("hi")
+        return (
+            <table
+                style={{ "borderCollapse": "collapse", "padding": "5px", "width": "100%", "border": "1px solid black" }}
+                className="table table-hover">
+                <thead style={{ "borderCollapse": "collapse", "padding": "5px", "width": "100%", "border": "1px solid black" }}>
+                    <tr style={{ "borderCollapse": "collapse", "padding": "5px", "width": "100%", "border": "1px solid black" }}>
+                        <th> Event name</th>
+                        <th> Address </th>
+                        <th> Event Date </th>
+                        {/* <th>Link to Sign-up</th> */}
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
+        )
+    }
 
     return (
         <div>
-            {<Calendar onClickDay={onChange} value={value}                
-            />}
-            
+            <Calendar onChange={setDate} value={date} />
+            <button type="button" onClick={fetchItineraryAndDisplay}>See Itineraries</button>           
         </div>
     );
 }
