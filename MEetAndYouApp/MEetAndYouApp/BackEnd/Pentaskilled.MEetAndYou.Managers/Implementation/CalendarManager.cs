031249cd6f3a4c5c74daf1aa8dc9ab29a498471a
Vivian Dinh <vivian.dinh03@student.csulb.edu>
2022-05-01 23:39:59 -0700

    fixed bug? maybe?

--- MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.Managers/Implementation/CalendarManager.cs
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.Managers/Implementation/CalendarManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pentaskilled.MEetAndYou.DataAccess;
+using Pentaskilled.MEetAndYou.DataAccess.Contracts;
 using Pentaskilled.MEetAndYou.Entities.DBModels;
 using Pentaskilled.MEetAndYou.Entities.Models;
 
@@ -10,12 +11,12 @@ namespace Pentaskilled.MEetAndYou.Managers
     public class CalendarManager : ICalendarManager
     {
         private readonly MEetAndYouDBContext _dbContext;
-        private readonly CalendarDAO _calendarDAO;
+        private readonly ICalendarDAO _calendarDAO;
 
-        public CalendarManager(MEetAndYouDBContext dBContext)
+        public CalendarManager(ICalendarDAO CalendarDAO, MEetAndYouDBContext dBContext)
         {
             _dbContext = dBContext;
-            _calendarDAO = new CalendarDAO();
+            _calendarDAO = CalendarDAO;
         }
 
 
