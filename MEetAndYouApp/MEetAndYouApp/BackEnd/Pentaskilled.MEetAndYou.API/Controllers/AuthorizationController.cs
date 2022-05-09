b90e58591c04cd29bd29cdf33bdf7ff2f1ce4f75
Brent Nishioka <brentnishioka@gmail.com>
2022-05-09 14:50:27 -0700

    Create AuthorizationController.cs

---
+++ MEetAndYouApp/MEetAndYouApp/BackEnd/Pentaskilled.MEetAndYou.API/Controllers/AuthorizationController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pentaskilled.MEetAndYou.Entities.DBModels;
+using Pentaskilled.MEetAndYou.Managers;
+
+namespace Pentaskilled.MEetAndYou.API.Controllers
+{
+    [Route("/api/Authorization")]
+    [ApiController]
+    public class AuthorizationController : ControllerBase
+    {
+        private readonly IAuthorizationManager _authzManager;
+
+        public AuthorizationController(IAuthorizationManager authzManager)
+        {
+            _authzManager = authzManager;
+        }
+
+        [HttpGet]
+        [Route("ValidateUserCredentials")]
+        public ActionResult<bool> ValidateUserCredentials(int userID, string userToken, string role)
+        {
+            bool response;
+            try
+            {
+                response = _authzManager.IsAuthorized(userID, userToken, role);
+            }
+            catch (Exception ex)
+            {
+                response = false;
+            }
+            return response;
+        }
+    }
+}
